131b7377c553c975b3d1c983855a583611ea2f30
Amanda Walters <awalters8@gatech.edu>
2020-02-14 13:30:03 -0500

    Add initial html and update listings for chapter 16

--- listings/listing_16_1.m
+++ listings/listing_16_1.m
@@ -7,13 +7,19 @@ end
 function b = insertionsort(a)
     % This function sorts a column vector,
     % using the insertion sort algorithm
+    
+    % Initializes the result and the while loop parameters
     b = []; i = 1; sz = length(a);
     while i <= sz
+        % Calls the helper function to insert the later value into the
+        % output vector
         b = insert(b, a(i) );
         i = i + 1;
     end
 end
 
+% The helper function that inserts a new value into a vector and returns
+% that vector
 function a = insert(a, v)
     % insert the value v into column vector a
     i = 1; sz = length(a); done = false;
