27a18d2fa231dfd88d27fe2b3cd55f5231c4fef4
Amanda Walters <awalters8@gatech.edu>
2020-02-20 23:17:42 -0500

    Update exercise and html syntax

--- exercises/exercise_13_1.m
+++ exercises/exercise_13_1.m
@@ -1,9 +1,1 @@
 % Exercise 13.1
-pic = imread('<your_favorite_image.jpg');
-[rows, cols, clrs] = size(pic);
-imshow(pic(2:2:end, 3:3:end, :));
-RFactor = 1.43; CFactor = 0.75; % shrink / stretch factors
-rowVec = round(linspace(1, rows, Rfactor*rows));
-colVec = round(linspace(1, cols, Cfactor*cols));
-imshow(pic(rowVec, colVec,:)); % shrunk / stretched image
-imshow(pic(:, :, [2 3 1])); % re-ordering the color layers
